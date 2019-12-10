d2be132b1f07a970795ec5361962c9d6446ad8c6
Brian Cavalier <brian@briancavalier.com>
2019-12-09 20:33:38 -0500

    Loosen unsafeRunEffects type

--- src/run.ts
+++ src/run.ts
@@ -1,5 +1,5 @@
 import { Cancel, runResume, uncancelable } from './env'
 import { Computation, runComputation } from './computation'
 
-export const unsafeRunEffects = <N> (c: Computation<never, void, N>): Cancel =>
+export const unsafeRunEffects = <N> (c: Computation<never, unknown, N>): Cancel =>
   runResume(runComputation(c)({} as never), _ => uncancelable)
