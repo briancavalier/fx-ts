6e5fe7b2b6c94bfedcea848f6de3fe4682410388
Brian Cavalier <brian@briancavalier.com>
2020-04-01 15:36:57 -0400

    Simplify catchFail

--- src/fail.ts
+++ src/fail.ts
@@ -10,13 +10,12 @@ export const attempt = <C extends Fail, A>(fx: Fx<C, A>) =>
 
 export const catchFail = <C1 extends Fail, C2, A, B>(fx: Fx<C1, A>, f: (e: Error) => Fx<C2, B>) =>
   op((c): Resume<A | B> => resume(k => {
-    const handle = {
-      fail: (e: Error) => {
-        cancel()
-        return resume(() => runFxWith(f(e), c, k))
-      }
+    const fail = (e: Error) => {
+      cancel()
+      return resume(() => runFxWith(f(e), c, k))
     }
+
     let cancel = uncancelable
-    cancel = runFxWith(fx, { ...c as any, ...handle }, k)
+    cancel = runFxWith(fx, { ...c as any, fail }, k)
     return cancel
   })) as Fx<Use<C1, Fail> & C2, A | B>
