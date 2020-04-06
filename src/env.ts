4102d66faeb0fc0e503a62a74021ffc11141c338
Brian Cavalier <brian@briancavalier.com>
2020-04-06 08:50:47 -0400

    Update comment

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,6 @@
 // An Env requires a set of capabilities C to compute a result A.
-// It would be nice to allow a more polymorphic result instead of
-// locking in the answer types to Cancel, Cancel.  I haven't
-// figured out a nice way that doesn't cause type issues when
-// mixing sync effects and async effects which require cancelability.
+// Allows using a continuation for flexibility or returning directly
+// for efficiency and stack safety.
 export type Env<C, A> = (c: C) => Resume<A>
 
 export type Intersect<U> =
