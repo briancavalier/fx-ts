9f62b80eda664c1e4e7a800977cba40eab3a79b6
Brian Cavalier <brian@briancavalier.com>
2020-10-28 21:38:50 -0400

    Add eslint and prettier tooling

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript'
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  env: {
+    node: true
+  },
+  rules: {
+    '@typescript-eslint/no-use-before-define': 'off',
+    'import/order': ["error", {
+      "newlines-between": "always",
+      alphabetize: {
+        order: 'asc',
+        caseInsensitive: false
+      }
+    }],
+    // Enable sort-imports to sort named imports within a single import
+    // statement, but *disable* its declaration sort, and let
+    // import/order's alphabetize feature handle sorting declarations
+    // based on import path.
+    'sort-imports': ['error', {
+      'ignoreDeclarationSort': true,
+    }],
+  },
+  settings: {
+    'import/resolver': {
+      // use <root>/tsconfig.json
+      typescript: {
+        alwaysTryTypes: true // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+      }
+    }
+  }
+}
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 120,
+  useTabs: false,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  semi: false,
+  htmlWhitespaceSensitivity: 'ignore'
+}
